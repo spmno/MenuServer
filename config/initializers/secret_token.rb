7e17f6699955d77994c8297daddaba004dab5b5e
spmno <sunqingpeng@hotmail.com>
2013-12-19 22:07:49 +0800

    add css for application header.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MenuServer::Application.config.secret_token = '1db7d4f068d5bf652efe94863b4c979fd29a2dff542f2723c1fb25a44158bc69de7d9b003c037a267a88810843e706a679cbf98b601a1a6bd766e645166054ef'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MenuServer::Application.config.secret_key_base = '0037ee8d90ab88bc7ef3d921852acc16cdcb9e36f2da8a533dbb044daf3233c28fdf1aabace7639a805ad6e1ee54fd6a93150472b06a00aa42945d9d2fd3de38'
--- db/schema.rb
+++ db/schema.rb
@@ -9,77 +9,77 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130717134006) do
+ActiveRecord::Schema.define(version: 20130717134006) do
 
-  create_table "dish_kinds", :force => true do |t|
+  create_table "dish_kinds", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "photo"
   end
 
-  create_table "dishes", :force => true do |t|
+  create_table "dishes", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price"
     t.integer  "dish_kind_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "photo"
   end
 
-  create_table "display_items", :force => true do |t|
+  create_table "display_items", force: true do |t|
     t.integer  "x"
     t.integer  "y"
     t.integer  "width"
     t.integer  "height"
     t.integer  "dish_id"
     t.integer  "page_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "members", :force => true do |t|
+  create_table "members", force: true do |t|
     t.string   "name"
     t.decimal  "balance"
     t.integer  "level_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "order_items", :force => true do |t|
+  create_table "order_items", force: true do |t|
     t.integer  "order_id"
     t.integer  "dish_id"
     t.integer  "discount"
     t.integer  "count"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "orders", :force => true do |t|
+  create_table "orders", force: true do |t|
     t.decimal  "total_price"
     t.datetime "order_time"
     t.datetime "checkout_time"
     t.integer  "member_id"
     t.integer  "table_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
-  create_table "page_versions", :force => true do |t|
+  create_table "page_versions", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "pages", :force => true do |t|
+  create_table "pages", force: true do |t|
     t.string   "name"
     t.string   "photo"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "dish_kind_id"
   end
 
